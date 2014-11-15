2ccc02e1e9133e7ee8920206a43099bf6e1cbae3
rebdog1337 <mike2777@hotmail.co.uk>
2014-11-15 10:22:31 +0000

    Added to source control

---
+++ Operator Sign Off Sheet/Operator Sign Off Sheet/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Operator_Sign_Off_Sheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CSM1 = new System.Windows.Forms.PictureBox();
+            this.buttonLoadJob = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBoxBatchNumbers = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxJobDetails = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.checkBoxPreFirstComplete = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonPreFirstWizard = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CSM1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Board Info and Date";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // CSM1
+            // 
+            this.CSM1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CSM1.ErrorImage = null;
+            this.CSM1.Image = global::Operator_Sign_Off_Sheet.Properties.Resources.CSM__1_;
+            this.CSM1.Location = new System.Drawing.Point(604, 3);
+            this.CSM1.Name = "CSM1";
+            this.tableLayoutPanel1.SetRowSpan(this.CSM1, 2);
+            this.CSM1.Size = new System.Drawing.Size(296, 164);
+            this.CSM1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CSM1.TabIndex = 3;
+            this.CSM1.TabStop = false;
+            this.CSM1.Click += new System.EventHandler(this.CSM1_Click);
+            // 
+            // buttonLoadJob
+            // 
+            this.buttonLoadJob.Location = new System.Drawing.Point(129, 129);
+            this.buttonLoadJob.Name = "buttonLoadJob";
+            this.buttonLoadJob.Size = new System.Drawing.Size(101, 23);
+            this.buttonLoadJob.TabIndex = 5;
+            this.buttonLoadJob.Text = "Load Job";
+            this.buttonLoadJob.UseVisualStyleBackColor = true;
+            this.buttonLoadJob.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(963, 512);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Next";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBoxBatchNumbers
+            // 
+            this.comboBoxBatchNumbers.DisplayMember = "BatchNumber";
+            this.comboBoxBatchNumbers.FormattingEnabled = true;
+            this.comboBoxBatchNumbers.Location = new System.Drawing.Point(3, 129);
+            this.comboBoxBatchNumbers.Name = "comboBoxBatchNumbers";
+            this.comboBoxBatchNumbers.Size = new System.Drawing.Size(120, 21);
+            this.comboBoxBatchNumbers.TabIndex = 13;
+            this.comboBoxBatchNumbers.ValueMember = "JobID";
+            this.comboBoxBatchNumbers.SelectedIndexChanged += new System.EventHandler(this.comboBoxBatchNumbers_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.96896F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.66076F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.37029F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonLoadJob, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxBatchNumbers, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CSM1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.24778F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.540107F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.21212F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(903, 596);
+            this.tableLayoutPanel1.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 2);
+            this.groupBox1.Controls.Add(this.richTextBoxJobDetails);
+            this.groupBox1.Location = new System.Drawing.Point(3, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(595, 420);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Job Details";
+            // 
+            // richTextBoxJobDetails
+            // 
+            this.richTextBoxJobDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxJobDetails.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxJobDetails.Location = new System.Drawing.Point(9, 19);
+            this.richTextBoxJobDetails.Name = "richTextBoxJobDetails";
+            this.richTextBoxJobDetails.ReadOnly = true;
+            this.richTextBoxJobDetails.Size = new System.Drawing.Size(347, 380);
+            this.richTextBoxJobDetails.TabIndex = 0;
+            this.richTextBoxJobDetails.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox2.Location = new System.Drawing.Point(604, 173);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 420);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Job Progress";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.checkBox2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox4, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox5, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox6, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox7, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox8, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox3, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(290, 401);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // checkBoxPreFirstComplete
+            // 
+            this.checkBoxPreFirstComplete.AutoSize = true;
+            this.checkBoxPreFirstComplete.Location = new System.Drawing.Point(6, 19);
+            this.checkBoxPreFirstComplete.Name = "checkBoxPreFirstComplete";
+            this.checkBoxPreFirstComplete.Size = new System.Drawing.Size(106, 17);
+            this.checkBoxPreFirstComplete.TabIndex = 1;
+            this.checkBoxPreFirstComplete.Text = "Pre 1st Complete";
+            this.checkBoxPreFirstComplete.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 103);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 203);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(148, 3);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(148, 103);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(3, 303);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(148, 203);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(80, 17);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(148, 303);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(80, 17);
+            this.checkBox8.TabIndex = 8;
+            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.buttonPreFirstWizard);
+            this.groupBox3.Controls.Add(this.checkBoxPreFirstComplete);
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(139, 94);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pre 1st Board";
+            // 
+            // buttonPreFirstWizard
+            // 
+            this.buttonPreFirstWizard.Location = new System.Drawing.Point(6, 42);
+            this.buttonPreFirstWizard.Name = "buttonPreFirstWizard";
+            this.buttonPreFirstWizard.Size = new System.Drawing.Size(127, 23);
+            this.buttonPreFirstWizard.TabIndex = 2;
+            this.buttonPreFirstWizard.Text = "Pre-1st Wizard";
+            this.buttonPreFirstWizard.UseVisualStyleBackColor = true;
+            this.buttonPreFirstWizard.Click += new System.EventHandler(this.buttonPreFirstWizard_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 596);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CSM1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox CSM1;
+        private System.Windows.Forms.Button buttonLoadJob;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBoxBatchNumbers;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.CheckBox checkBoxPreFirstComplete;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.RichTextBox richTextBoxJobDetails;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonPreFirstWizard;
+    }
+}
+
---
+++ Operator Sign Off Sheet/Operator Sign Off Sheet/Form1.cs
@@ -0,0 +1,143 @@
+﻿using Operator_Sign_Off_Sheet.Database;
+using Operator_Sign_Off_Sheet.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Operator_Sign_Off_Sheet.Views;
+
+namespace Operator_Sign_Off_Sheet
+{
+    public partial class Form1 : Form
+    {
+        int _jobID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxBatchNumbers.Items.AddRange(GetJobsFromDB().ToArray());
+            CommonMethods common = new CommonMethods();
+            var items = DatabaseInterface.GetAllJobs();
+
+            var itemsInList = common.JobObjectsFromDataTable(items);
+
+            foreach (DataRow item in items.Rows)
+            {
+                MessageBox.Show(item.Field<int>("BatchNumber").ToString());
+            }
+
+            itemsInList[0].Date = DateTime.Now;
+
+            bool success = DatabaseInterface.UpdateJob(itemsInList[0]);
+
+            var items2 = DatabaseInterface.GetAllJobs();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            
+            DateTime theDate = DateTime.UtcNow;
+
+            string custom = theDate.ToString("U");
+
+            MessageBox.Show(custom);
+
+
+        }
+
+        private void CSM1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Pre1stBoard newBoard = new Pre1stBoard();
+            newBoard.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBoxBatchNumbers.SelectedItem != null)
+            {
+                LoadJob(_jobID);
+            }
+        }
+
+        private void LoadJob(int JobID)
+        {
+            // clear the list of current selected job (if one)
+            richTextBoxJobDetails.Text = "";
+            // create connection to our database
+            using (LinqToOlgaDBDataContext con = new LinqToOlgaDBDataContext())
+            {
+                // SQL query using linq that will find a job in the database that maches the select JobID
+                var getTheJob = from a in con.JobsTbls
+                                where a.JobID == JobID
+                                select a;
+
+                
+                    JobDetails jon = new JobDetails()
+                    {
+                        JobFromDB = getTheJob.First()
+                    };
+
+                    richTextBoxJobDetails.Text = jon.ToString();
+                
+
+
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get list of current jobs that are in database
+        }
+
+        /// <summary>
+        /// Gets the current jobs from the database
+        /// </summary>
+        /// <returns></returns>
+        private List<JobsTbl> GetJobsFromDB()
+        {
+            using (LinqToOlgaDBDataContext con = new LinqToOlgaDBDataContext())
+            {
+                // get all jobs from the database
+                return con.JobsTbls.ToList();
+            }
+        }
+
+        private void comboBoxBatchNumbers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // check if the user has selected an item before doing something with it
+            if (comboBoxBatchNumbers.SelectedItem != null)
+            {
+                // cast the selected item as a JobTbl object then save the JobID in a varible
+                _jobID = ((JobsTbl)comboBoxBatchNumbers.SelectedItem).JobID;
+            }
+        }
+
+        private void buttonPreFirstWizard_Click(object sender, EventArgs e)
+        {
+            PreFirstBoardWizard pre1stWizard = new PreFirstBoardWizard();
+            pre1stWizard.ShowDialog();
+        }
+    }
+}
